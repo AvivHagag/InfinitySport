96105ceda0838c955a96625fee37ef8fd64ae890
Daniel Arvili <danielarvili@Daniels-MacBook-Pro.local>
2024-02-20 18:45:05 +0200

    added footer and link

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -6,6 +6,7 @@ import Providers from "./navbar/Providers";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import { ThemeProvider } from "./theme-provider";
+import Footer from "./Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default async function RootLayout({
             <main className="flex flex-col w-full text-xl">
               <Navbar />
               <div className="flex-grow">{children}</div>
+              <Footer />
             </main>
           </Providers>
         </ThemeProvider>
