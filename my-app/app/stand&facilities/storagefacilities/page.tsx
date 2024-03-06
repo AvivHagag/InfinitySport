61ef86f265c5206635922b42d216824d5f158c7a
AvivHagag <Aviv1049@gmail.com>
2024-03-06 13:00:13 +0200

    finish all pages

--- my-app/app/stand&facilities/storagefacilities/page.tsx
+++ my-app/app/stand&facilities/storagefacilities/page.tsx
@@ -1,7 +1,17 @@
-export default function Home() {
+import { getAllProducts } from "../../ServerAction/ServerAction";
+import StorageFacilitiesComponent from "./StorageFacilitiesComponent";
+
+export default async function Home() {
+  const categoriesIDs = [12];
+  const StorageFacilitiesProducts = await getAllProducts(categoriesIDs);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>storage facilities</h1>
+    <main className="flex min-h-screen flex-col py-12 px-2">
+      <h1 className="text-center">Storage Facilities</h1>
+      {StorageFacilitiesProducts && (
+        <StorageFacilitiesComponent
+          StorageFacilitiesProducts={StorageFacilitiesProducts}
+        />
+      )}
     </main>
   );
 }
