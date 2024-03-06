419d972636c77f2b20e00e05168412b547217072
AvivHagag <Aviv1049@gmail.com>
2024-03-06 23:23:08 +0200

    change the modal a little

--- my-app/app/ProductModal.tsx
+++ my-app/app/ProductModal.tsx
@@ -19,18 +19,18 @@ export default function ProductModal({ product, onClose }: ProductModalProps) {
       className="fixed inset-0 z-50 bg-gray-600 dark:bg-slate-900 bg-opacity-80 dark:bg-opacity-80 w-full overflow-y-auto h-full w-full"
       id="my-modal"
     >
-      <div className="relative top-20 mx-auto p-1 border shadow-lg h-[394px] w-3/4 rounded-xl bg-white dark:bg-slate-950">
+      <div className="relative top-20 mx-auto p-1 border shadow-lg h-4/5 md:h-[394px] w-4/5 sm:w-3/4 rounded-xl bg-white dark:bg-slate-950">
         <div className="absolute right-0 top-0 p-1">
           <XMarkIcon className="h-6 w-6" onClick={onClose} />
         </div>
-        <div className="flex flex-row w-auto">
-          <div className="relative w-96 h-96 flex-shrink-0">
+        <div className="flex flex-col sm:flex-row w-auto">
+          <div className="relative hidden md:block sm:w-96 sm:h-96 flex-shrink-0">
             {product.image && (
               <Image
                 src={product.image}
                 alt={product.name}
                 fill
-                className="rounded-tl-lg rounded-bl-lg"
+                className="sm:rounded-tl-lg sm:rounded-bl-lg"
                 style={{
                   objectFit: "fill",
                 }}
@@ -38,26 +38,76 @@ export default function ProductModal({ product, onClose }: ProductModalProps) {
             )}
           </div>
           <div className="flex flex-grow flex-col mt-3 px-1">
-            <h2 className="text-lg font-medium capitalize text-naivyBlue dark:text-glowGreen">
-              {product.name}, {product.manufacturer}
-            </h2>
-            <div className="py-1 flex items-center">
-              <p
-                className={`text-naivyBlue dark:text-glowGreen text-base ${
-                  product.onSale ? "line-through" : ""
-                }`}
-              >
-                ${product.price.toFixed(2)}
-              </p>
-              {product.onSale && product.salePercent && (
-                <p className="text-gray-700 dark:text-white text-lg sm:ml-2">
-                  $
-                  {(
-                    (product.price * (100 - product.salePercent)) /
-                    100
-                  ).toFixed(2)}
+            <div className="hidden md:block">
+              <h2 className="text-lg font-medium capitalize text-naivyBlue dark:text-glowGreen">
+                {product.name}, {product.manufacturer}
+                {product.onSale && (
+                  <span className="text-base mx-4 p-1 rounded bg-naivyBlue dark:bg-glowGreen text-white dark:text-black">
+                    Sale!
+                  </span>
+                )}
+              </h2>
+              <div className="py-1 flex items-center">
+                <p
+                  className={`text-naivyBlue dark:text-glowGreen text-base ${
+                    product.onSale ? "line-through" : ""
+                  }`}
+                >
+                  ${product.price.toFixed(2)}
                 </p>
-              )}
+                {product.onSale && product.salePercent && (
+                  <p className="text-gray-700 dark:text-white text-lg sm:ml-2">
+                    $
+                    {(
+                      (product.price * (100 - product.salePercent)) /
+                      100
+                    ).toFixed(2)}
+                  </p>
+                )}
+              </div>
+            </div>
+            <div className="flex flex-row items-center md:hidden pt-3 justify-between">
+              <div className="flex flex-col md:hidden">
+                <h2 className="text-lg font-medium capitalize text-naivyBlue dark:text-glowGreen">
+                  {product.name}, {product.manufacturer}
+                  {product.onSale && (
+                    <span className="text-base mx-4 p-1 rounded bg-naivyBlue dark:bg-glowGreen text-white dark:text-black">
+                      Sale!
+                    </span>
+                  )}
+                </h2>
+                <div className="py-1 flex items-center">
+                  <p
+                    className={`text-naivyBlue dark:text-glowGreen text-base ${
+                      product.onSale ? "line-through" : ""
+                    }`}
+                  >
+                    ${product.price.toFixed(2)}
+                  </p>
+                  {product.onSale && product.salePercent && (
+                    <p className="text-gray-700 dark:text-white text-lg sm:ml-2">
+                      $
+                      {(
+                        (product.price * (100 - product.salePercent)) /
+                        100
+                      ).toFixed(2)}
+                    </p>
+                  )}
+                </div>
+              </div>
+              <div className="relative w-24 h-24 flex-shrink-0">
+                {product.image && (
+                  <Image
+                    src={product.image}
+                    alt={product.name}
+                    className="rounded-lg"
+                    fill
+                    style={{
+                      objectFit: "fill",
+                    }}
+                  />
+                )}
+              </div>
             </div>
             <div className="mt-1 text-sm ">
               <span className="text-base">Description :</span>{" "}
