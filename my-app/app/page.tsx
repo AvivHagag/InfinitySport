a5af70d44f2937d62f43b75ed8694ab84c8c9a1e
AvivHagag <Aviv1049@gmail.com>
2024-03-28 14:26:56 +0200

    finish Carousel

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,10 @@
-import AdsComponent from "./IndexComponents/AdsComponent";
-
+import AdsComponent from "./Main/IndexComponents/AdsComponent";
+import Carousel from "./Main/CarouselPopular/page";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex flex-col items-center justify-between">
       <AdsComponent />
-      <h1>Hello world</h1>
+      <Carousel />
     </main>
   );
 }
